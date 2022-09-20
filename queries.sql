83c35a1552d41c2104cdde63eb85c695894c1afd
AngomRanjan <angomranjan@gmail.com>
2022-09-20 15:13:06 +0530

    Update schema.sql

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name LIKE '%mon%';
+SELECT * from animals WHERE name LIKE '%mon';
 SELECT name from animals WHERE date_of_birth BETWEEN '2016-1-1' AND '2019-12-31';
 SELECT * from animals WHERE neutered IS TRUE AND escape_attempts < 3;
 SELECT date_of_birth from animals WHERE name IN ('Agumon','Pikachu');
