7c3ad0b8160654086f1dec6d830bb53cf7679809
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-09-28 13:46:55 +0430

    REVERTING ALL CHANGES

--- queries.sql
+++ queries.sql
@@ -117,6 +117,4 @@ JOIN animals ON visits.animals_id = animals.id
 JOIN species ON species.id = animals.species_id
 WHERE vets.name = 'Maisy Smith' GROUP BY species.name ORDER BY count DESC LIMIT 1;
 
-EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animalS_id = 4;
-EXPLAIN ANALYZE SELECT * FROM visits where vetS_id = 2;
-EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
+
